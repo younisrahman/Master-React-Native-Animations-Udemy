80e974d68e1179833a451342a20f71461426918d
Younis <younisrahman007@gmail.com>
2022-07-10 21:56:34 +0600

    Translste

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Opacity from './src/Topics/Opacity';
+import TranslatePosition from './src/Topics/TranslatePosition';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style='auto' />
-      <Opacity />
+      {/* <Opacity /> */}
+      <TranslatePosition />
     </View>
   );
 }
