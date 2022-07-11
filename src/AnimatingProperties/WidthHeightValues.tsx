38e82675f951a082982625d059391cc303350220
Younis <younisrahman007@gmail.com>
2022-07-11 17:44:22 +0600

    useNativeDriver: true,

--- src/AnimatingProperties/WidthHeightValues.tsx
+++ src/AnimatingProperties/WidthHeightValues.tsx
@@ -1,1 +1,52 @@
-git;
+import React, { useRef, useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Animated,
+  TouchableWithoutFeedback,
+  Text,
+} from 'react-native';
+
+const Opacity = () => {
+  const animation = useRef(new Animated.Value(1)).current;
+  const startAnimation = () => {
+    Animated.timing(animation, {
+      toValue: -2,
+      duration: 1500,
+      useNativeDriver: true,
+    }).start(() => {
+      Animated.timing(animation, {
+        toValue: 2,
+        duration: 1500,
+        useNativeDriver: true,
+      }).start();
+    });
+  };
+  const animatedStyles = {
+    transform: [{ scaleY: animation }],
+  };
+  return (
+    <View>
+      <TouchableWithoutFeedback onPress={startAnimation}>
+        <Animated.View style={[styles.box, animatedStyles]}>
+          <Text>This side forward</Text>
+        </Animated.View>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: 'tomato',
+  },
+});
+
+export default Opacity;
